8c21ab2dd60a873ecc97e40b67e89ce3633ddf50
breyes4 <71798440+breyes4@users.noreply.github.com>
2023-12-06 17:35:08 -0600

    Address.java

--- src/composition/Address.java
+++ src/composition/Address.java
@@ -1,48 +1,12 @@
 package composition;
 
-public class Address {
+public interface Address {
 
-    private String streetAddress;
-    private int zipCode;
-    private String state;
-    private String phone;
+    public String getPhone();
 
-    public Address(String streetAddress, int zipCode, String state, String phone) {
-        this.streetAddress = streetAddress;
-        this.zipCode = zipCode;
-        this.state = state;
-        this.phone = phone;
-    }
+    public String getState();
 
-    public String getPhone() {
-        return phone;
-    }
+    public String getStreetAddress();
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getStreetAddress() {
-        return streetAddress;
-    }
-
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
-    }
-
-    public int getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
-    }
+    public int getZipCode();
 }
